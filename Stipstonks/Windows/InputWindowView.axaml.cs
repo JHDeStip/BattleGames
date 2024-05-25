26105ac7ce2c2eb6081636e9dd106c2497a51c39
Stan Wijckmans <stan.wijckmans@aviobook.aero>
2024-05-25 18:19:07 +0200

    Prevent input items scroll viewer from resetting its position when input items are refreshed.

--- Stipstonks/Windows/InputWindowView.axaml.cs
+++ Stipstonks/Windows/InputWindowView.axaml.cs
@@ -1,10 +1,18 @@
+using Avalonia;
 using Stip.BattleGames.Common.Windows;
 
 namespace Stip.Stipstonks.Windows;
 
 public partial class InputWindowView : WindowBase<InputWindowViewModel>
 {
+    private Vector _lastScrollPosition = Vector.Zero;
+
     public InputWindowView(InputWindowViewModel viewModel)
         : base(viewModel)
-        => InitializeComponent();
+    {
+        InitializeComponent();
+
+        InputItemsScrollViewer.ScrollChanged += (_, _) => _lastScrollPosition = InputItemsScrollViewer.Offset;
+        InputItemsItemsControl.SizeChanged += (_, _) => InputItemsScrollViewer.Offset = _lastScrollPosition;
+    }
 }
